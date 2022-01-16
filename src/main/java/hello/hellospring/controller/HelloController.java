4103f6ffd6e6a8d0998e06833ede58f069eb25b3
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-01-16 19:52:10 +0900

    GetMapping using API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController
@@ -21,5 +22,12 @@ public class HelloController
     {
         model.addAttribute("name",name);    // "name"에 입력받은 String 파라미터를 매핑한다.
         return "hello-template";    // "hello-template.html" 페이지를 띄운다.
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // html의 body 부분에 데이터를 직접 넘겨주겠다.
+    public String helloString(@RequestParam("name") String name)
+    {
+        return "hello" + name;  // 문자열이 그대로 반환된다.
     }
 }
